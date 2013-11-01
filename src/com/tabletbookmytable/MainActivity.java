5bbb30d40dfd887f6d2837e053bea5ea2523653a
ankitnagda <ankit.akn@gmail.com>
2013-10-31 23:40:19 -0700

    Adding 3 screens

--- src/com/tabletbookmytable/MainActivity.java
+++ src/com/tabletbookmytable/MainActivity.java
@@ -2,7 +2,9 @@ package com.tabletbookmytable;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -12,6 +14,7 @@ public class MainActivity extends Activity
     {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	
     }
 
     @Override
@@ -20,6 +23,11 @@ public class MainActivity extends Activity
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.main, menu);
 	return true;
+    }
+    
+    public void checkIn(View view)
+    {
+    startActivity(new Intent(this, TapYourDeviceActivity.class));
     }
 
 }
--- src/com/tabletbookmytable/TapYourDeviceActivity.java
+++ src/com/tabletbookmytable/TapYourDeviceActivity.java
@@ -2,7 +2,9 @@ package com.tabletbookmytable;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class TapYourDeviceActivity extends Activity
 {
@@ -20,6 +22,11 @@ public class TapYourDeviceActivity extends Activity
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.tap_your_device, menu);
 	return true;
+    }
+    
+    public void testing(View view)
+    {
+    startActivity(new Intent(this, OptionsActivity.class));
     }
 
 }
