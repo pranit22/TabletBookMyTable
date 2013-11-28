7c5c7d55982906b14ba4fded9757de4994c8cc77
Pranit Mhatre <pranit22@gmail.com>
2013-11-28 15:24:48 -0800

    Changed theme and layout a bit more

--- src/com/tabletbookmytable/SubMenuActivity.java
+++ src/com/tabletbookmytable/SubMenuActivity.java
@@ -1,67 +1,60 @@
 package com.tabletbookmytable;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SubMenuActivity extends Activity
-{
+public class SubMenuActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_sub_menu);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_sub_menu);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.sub_menu, menu);
-	return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sub_menu, menu);
+        return true;
     }
-    
-    public void onApp(View view)
-    {
+
+    public void onApp(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 0);
         startActivity(intent);
     }
-    
-    public void onBev(View view)
-    {
+
+    public void onBev(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 1);
         startActivity(intent);
     }
-    
-    public void onSal(View view)
-    {
+
+    public void onSal(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 2);
         startActivity(intent);
     }
-    
-    public void onMain(View view)
-    {
+
+    public void onMain(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 3);
         startActivity(intent);
     }
-    
-    public void onPizza(View view)
-    {
+
+    public void onPizza(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 4);
         startActivity(intent);
     }
-    
-    public void onDess(View view)
-    {
+
+    public void onDess(View view) {
         Intent intent = new Intent(this, TabbedMenuActivity.class);
         intent.putExtra("tabIndex", 5);
         startActivity(intent);
