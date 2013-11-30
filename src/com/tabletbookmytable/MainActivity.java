f23b5f0e4146fae2162402ec59b85c12f096f204
Pranit Mhatre <pranit22@gmail.com>
2013-11-29 17:57:17 -0800

    NFC for Check-In! Yay!

--- src/com/tabletbookmytable/MainActivity.java
+++ src/com/tabletbookmytable/MainActivity.java
@@ -1,11 +1,14 @@
 package com.tabletbookmytable;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +17,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        if(getIntent().hasExtra("invalidCheckin")) {
+            String text = "Hi "+ getIntent().getStringExtra("user")+", please check if you are checking-in at the correct table.\n" +
+                    "Please wait, an attendant will assist you shortly.";
+
+            final Activity thisActivity = this;
+            new AlertDialog.Builder(this)
+                    .setTitle("Booking not found!")
+                    .setIcon(R.drawable.icon_exit)
+                    .setMessage(text)
+                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .show();
+        }
     }
 
     @Override
--- src/com/tabletbookmytable/OptionsActivity.java
+++ src/com/tabletbookmytable/OptionsActivity.java
@@ -2,28 +2,65 @@ package com.tabletbookmytable;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tabletbookmytable.extras.TabletBookMyTable;
 
 public class OptionsActivity extends Activity {
 
+    NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_options);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.options, menu);
-        return true;
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        String user = new String(msg.getRecords()[0].getPayload());
+
+        if(user.equals("Pranit") || user.equals("Guest")) {
+        ((TextView) findViewById(R.id.username)).setText("Welcome, " + user + "!");
+        }
+        else {
+            Intent newIntent = new Intent(this, MainActivity.class);
+            newIntent.putExtra("user", user);
+            newIntent.putExtra("invalidCheckin", true);
+            startActivity(newIntent);
+        }
     }
 
     public void orderFood(View view) {
